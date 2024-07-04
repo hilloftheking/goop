d3028bfc095ee90a600c449037ef5aa82c5e65fd
hilloftheking <91853499+hilloftheking@users.noreply.github.com>
2024-07-03 22:46:23 -0400

    Update gitignore

--- src/shader_sources.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-extern const char *COMPUTE_SDF_COMP_SRC;
-extern const char *RAYMARCH_FRAG_SRC;
-extern const char *RAYMARCH_VERT_SRC;
-extern const char *SKYBOX_FRAG_SRC;
-extern const char *SKYBOX_VERT_SRC;
